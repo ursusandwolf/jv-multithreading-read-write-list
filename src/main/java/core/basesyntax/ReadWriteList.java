cfaf12b033da83b27eea301f08a9ec5328678d9b
alex <ursusandwolf@protonmail.com>
2026-03-09 17:19:05 +0200

    finally {             rl.unlock();

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,24 +2,44 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
     private List<E> list = new ArrayList<>();
     private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private Lock wl = lock.writeLock();
+    private Lock rl = lock.readLock();
 
     public void add(E element) {
-        // write your code here
+        wl.lock();
+        try {
+            list.add(element);
+        } finally {
+            wl.unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        E element = null;
+        rl.lock();
+        try {
+            element = list.get(index);
+        } finally {
+            rl.unlock();
+        }
+        return element;
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        int i = 0;
+        rl.lock();
+        try {
+            return list.size();
+        } finally {
+            rl.unlock();
+            return i;
+        }
     }
 }
